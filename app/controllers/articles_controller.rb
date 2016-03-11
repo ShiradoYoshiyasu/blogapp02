39b4752a8e9ae17a866305a264f5c27188c525dd
Yoshiyasu Shirado <y-shirado@towatech.net>
2016-03-11 14:26:03 +0900

    edit with named_placeholder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,9 @@ class ArticlesController < ApplicationController
     search_mine = params[:search_mine]
     if q
       if q[:search_category] != ""
-        @articles = Article.includes(:category, :user).where('category_id = ? and (title LIKE ? or sentence LIKE ?)',"#{q[:search_category]}", "%#{q[:search_string]}%", "%#{q[:search_string]}%").page(params[:page]).per(10)
+        @articles = Article.includes(:category, :user).where('category_id =  :category and (title LIKE :string or sentence LIKE :string)', category: "#{q[:search_category]}", string: "%#{q[:search_string]}%").page(params[:page]).per(10)
       else
-        @articles = Article.includes(:category, :user).where('title LIKE ? or sentence LIKE ?', "%#{q[:search_string]}%", "%#{q[:search_string]}%").page(params[:page]).per(10)
+        @articles = Article.includes(:category, :user).where('title LIKE :string or sentence LIKE :string', string: "%#{q[:search_string]}%").page(params[:page]).per(10)
       end
     elsif search_mine
       @articles = Article.includes(:category, :user).where('user_id = ?', "#{search_mine}").page(params[:page]).per(10)
