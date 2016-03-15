dffbdca663b93c672cf2d07874bcd7e1d6ab7b81
Yoshiyasu Shirado <y-shirado@towatech.net>
2016-03-15 18:29:34 +0900

    add validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,15 @@
 class Article < ActiveRecord::Base
-  
+
   belongs_to :category
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validates :title, presence: { message: "タイトルを入力してください"}
-  validates :sentence, presence: { message: "本文を入力してください"}
+  validates :title, presence: { message: "タイトルを入力してください"},
+            length: { maximum: 30, message:"本文は30字以内にしてください" },
+            uniqueness: { scope: [:user_id], message:"過去に同一タイトルの投稿があります"}
+  validates :sentence, presence: { message: "本文を入力してください"},
+            length: { minimum: 10, message: "本文は10文字以上にしてください",
+                      maximum: 1000, message:"本文は1000字以内にしてください" }
   validates :category_id, presence: { message: "カテゴリーを選択してください"}
-  validates :user_id, presence: { message: "ログインしてください"}
 
   scope :search_by_category, -> (category){where('category_id = ?', category) if category.present?}
   scope :search_by_string, -> (string){where('title LIKE :string or sentence LIKE :string', string: "%#{string}%" ) if string.present?}
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :articles, dependent: :destroy
-  validates :name, presence: { message: "カテゴリー名を入力してください"}
+  validates :name, presence: { message: "カテゴリー名を入力してください" },
+            uniqueness: { message:"名称が重複しています" }
 end
